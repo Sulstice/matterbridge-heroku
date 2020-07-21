83d8f22b7321e73184354140d0cfaa185674655c
Sulstice <sharifsuliman1@gmail.com>
2020-07-20 19:20:11 -0500

    ss/python-package-addition first pass at adding more resources (my stuff)
    ss/python-package-addition first pass at adding more resources (my stuff)

--- start.sh
+++ start.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 ###############################################################################################
-# Apply some defaults values, and remove quotes that Heroku adds to truthy and numeric values #
-###############################################################################################
-
-export IRC_USE_TLS=`echo ${UseIRC_USE_TLSTLS:=true} | tr -d \"`
-export IRC_USE_SASL=`echo ${IRC_USE_SASL:=true} | tr -d \"`
-export IRC_SKIP_TLS_VERIFY=`echo ${IRC_SKIP_TLS_VERIFY:=false} | tr -d \"`
-export MATTERMOST_NO_TLS=`echo ${MATTERMOST_NO_TLS:=false} | tr -d \"`
-export MATTERMOST_SKIP_TLS_VERIFY=`echo ${MATTERMOST_SKIP_TLS_VERIFY:=false} | tr -d \"`
-export MATTERMOST_SHOW_JOIN_PART=`echo ${MATTERMOST_SHOW_JOIN_PART:=false} | tr -d \"`
-export MATTERMOST_PREFIX_MSG_WITH_NICK=`echo ${MATTERMOST_PREFIX_MSG_WITH_NICK:=false} | tr -d \"`
-export MATTERMOST_NICKS_PER_ROW=`echo ${MATTERMOST_NICKS_PER_ROW:=4} | tr -d \"`
+## Apply some defaults values, and remove quotes that Heroku adds to truthy and numeric values #
+################################################################################################
+#
+#export IRC_USE_TLS=`echo ${UseIRC_USE_TLSTLS:=true} | tr -d \"`
+#export IRC_USE_SASL=`echo ${IRC_USE_SASL:=true} | tr -d \"`
+#export IRC_SKIP_TLS_VERIFY=`echo ${IRC_SKIP_TLS_VERIFY:=false} | tr -d \"`
+#export MATTERMOST_NO_TLS=`echo ${MATTERMOST_NO_TLS:=false} | tr -d \"`
+#export MATTERMOST_SKIP_TLS_VERIFY=`echo ${MATTERMOST_SKIP_TLS_VERIFY:=false} | tr -d \"`
+#export MATTERMOST_SHOW_JOIN_PART=`echo ${MATTERMOST_SHOW_JOIN_PART:=false} | tr -d \"`
+#export MATTERMOST_PREFIX_MSG_WITH_NICK=`echo ${MATTERMOST_PREFIX_MSG_WITH_NICK:=false} | tr -d \"`
+#export MATTERMOST_NICKS_PER_ROW=`echo ${MATTERMOST_NICKS_PER_ROW:=4} | tr -d \"`
 
 ########################################################################
 # Write Config variables in envrionment to the configuration JSON file #
 ########################################################################
+mkdir lib
+curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-`uname -s`-`uname -m` -o lib/envsubst
+chmod +x lib/envsubst
 lib/envsubst < config/config-heroku-template.toml > config/config-heroku.toml
-
-#####################################
+#
+######################################
 # Pass SIGTERM to Matterbridge proc #
 #####################################
 function _term {
